08564bc94eee9fe68e46c026958cf7030af559bd
Victor Plushko <vicplushko33@gmail.com>
2020-04-27 19:19:42 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -18,7 +18,7 @@ class GetRequester
   def parse_json
     programs = JSON.parse(self.get_response_body)
     programs.collect do |program|
-      program["agency"]
+      program[converted_data]
     end
   end
     
